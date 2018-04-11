1b070cda5275ab63be021a8e5cfeaabf2cc69405
Matheus Sartori <ms.sartori@outlook.com.br>
2018-04-11 11:38:07 -0300

    Express instalado + testes

---
+++ index.js
@@ -0,0 +1,10 @@
+var app = require('express')();
+
+app.listen(3000, function() {
+  console.log('Servidor rodando na porta 3000.');
+});
+
+app.get('/teste', function(req, res) {
+  console.log('Recebida requisição de teste na porta 3000.');
+  res.send('OK');
+});
