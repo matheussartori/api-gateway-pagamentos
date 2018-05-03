68ba5fac41c5f874db72a92d17eb94bcf0f5f341
Matheus Sartori <ms.sartori@outlook.com.br>
2018-05-03 15:05:27 -0300

    url para confirmar pagamento, refatoração de ident no controller pagamentos

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -1,42 +1,61 @@
-module.exports = function(app) {
-  app.get('/pagamentos', function(req, res) {
-    console.log('Recebida requisição de teste na porta 3000.');
-    res.send('OK');
-  });
-
-  app.post('/pagamentos/pagamento', function(req, res) {
-
-    req.assert('forma_de_pagamento',
-      'Forma de pagamento é obrigatório!').notEmpty();
-    req.assert('valor',
-      'Valor é obrigatório e deve ser decimal!').notEmpty().isFloat();
-
-    var erros = req.validationErrors();
-    if(erros) {
-      console.log('Erros de validação encontrados.');
-      res.status(400).send(erros);
-      return;
-    }
-
-  	var pagamento = req.body;
-  	console.log('Processando uma requisição de um novo pagamento...');
-
-  	pagamento.status = 'CRIADO';
-  	pagamento.data = new Date;
-
-    var connection = app.persistence.connectionFactory();
-    var pagamentoDao = new app.persistence.pagamentos.PagamentoDao(connection);
-
-    pagamentoDao.salva(pagamento, function(error, result) {
-      if(error) {
-        console.log('Erro ao inserir no banco: ' + error);
-        res.status(500).send(error);
-      } else {
-        console.log('Pagamento criado');
-        res.location('/pagamentos/pagamento/' + result.insertId);
-        res.status(201).json(pagamento);
-      }
+module.exports = function (app) {
+    app.get('/pagamentos', function (req, res) {
+        console.log('Recebida requisição de teste na porta 3000.');
+        res.send('OK');
     });
 
-  });
+    app.put('/pagamentos/pagamento/:id', function (req, res) {
+        var pagamento = {};
+        var id = req.params.id;
+
+        pagamento.id = id;
+        pagamento.status = 'CONFIRMADO';
+
+        var connection = app.persistence.connectionFactory();
+        var pagamentoDao = new app.persistence.pagamentos.PagamentoDao(connection);
+
+        pagamentoDao.atualiza(pagamento, function (error) {
+            if (error) {
+                res.status(500).send(error);
+                return;
+            }
+            res.send(pagamento);
+        });
+    });
+
+    app.post('/pagamentos/pagamento', function (req, res) {
+
+        req.assert('forma_de_pagamento',
+            'Forma de pagamento é obrigatório!').notEmpty();
+        req.assert('valor',
+            'Valor é obrigatório e deve ser decimal!').notEmpty().isFloat();
+
+        var erros = req.validationErrors();
+        if (erros) {
+            console.log('Erros de validação encontrados.');
+            res.status(400).send(erros);
+            return;
+        }
+
+        var pagamento = req.body;
+        console.log('Processando uma requisição de um novo pagamento...');
+
+        pagamento.status = 'CRIADO';
+        pagamento.data = new Date;
+
+        var connection = app.persistence.connectionFactory();
+        var pagamentoDao = new app.persistence.pagamentos.PagamentoDao(connection);
+
+        pagamentoDao.salva(pagamento, function (error, result) {
+            if (error) {
+                console.log('Erro ao inserir no banco: ' + error);
+                res.status(500).send(error);
+            } else {
+                console.log('Pagamento criado');
+                res.location('/pagamentos/pagamento/' + result.insertId);
+                res.status(201).json(pagamento);
+            }
+        });
+
+    });
 }
