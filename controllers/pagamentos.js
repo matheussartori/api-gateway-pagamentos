cde5280351980502c9cfbfbaf7e54779eb11d822
Matheus Sartori <ms.sartori@outlook.com.br>
2018-05-02 23:04:19 -0300

    validação do post em pagamentos com express-validator

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -5,6 +5,19 @@ module.exports = function(app) {
   });
 
   app.post('/pagamentos/pagamento', function(req, res) {
+
+    req.assert('forma_de_pagamento',
+      'Forma de pagamento é obrigatório!').notEmpty();
+    req.assert('valor',
+      'Valor é obrigatório e deve ser decimal!').notEmpty().isFloat();
+
+    var erros = req.validationErrors();
+    if(erros) {
+      console.log('Erros de validação encontrados.');
+      res.status(400).send(erros);
+      return;
+    }
+
   	var pagamento = req.body;
   	console.log('Processando uma requisição de um novo pagamento...');
 
@@ -16,7 +29,8 @@ module.exports = function(app) {
 
     pagamentoDao.salva(pagamento, function(error, result) {
       if(error) {
-        res.send(error);
+        console.log('Erro ao inserir no banco: ' + error);
+        res.status(400).send(error);
       } else {
         console.log('Pagamento criado');
         res.json(pagamento);
