1c3f5ab7b6ba9afe4ce86fc08c97a5fada9bc8b4
Matheus Sartori <ms.sartori@outlook.com.br>
2018-05-03 22:33:54 -0300

    rota para cancelar pagamentos e verbos http

--- controllers/pagamentos.js
+++ controllers/pagamentos.js
@@ -4,6 +4,26 @@ module.exports = function (app) {
         res.send('OK');
     });
 
+    app.delete('/pagamentos/pagamento/:id', function (req, res) {
+        var pagamento = {};
+        var id = req.params.id;
+
+        pagamento.id = id;
+        pagamento.status = 'CANCELADO';
+
+        var connection = app.persistence.connectionFactory();
+        var pagamentoDao = new app.persistence.pagamentos.PagamentoDao(connection);
+
+        pagamentoDao.atualiza(pagamento, function (error) {
+            if (error) {
+                res.status(500).send(error);
+                return;
+            }
+            console.log('Pagamento cancelado.');
+            res.status(204).send(pagamento);
+        });
+    });
+
     app.put('/pagamentos/pagamento/:id', function (req, res) {
         var pagamento = {};
         var id = req.params.id;
@@ -19,7 +39,8 @@ module.exports = function (app) {
                 res.status(500).send(error);
                 return;
             }
-            res.send(pagamento);
+            console.log('Pagamento confirmado.');
+            res.status(200).send(pagamento);
         });
     });
 
