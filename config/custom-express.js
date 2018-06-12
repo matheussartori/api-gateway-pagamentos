e78e5cf9c0a5e2f4705f3e026e75a90b402b8520
Matheus Sartori <ms.sartori@outlook.com.br>
2018-06-12 01:44:03 -0300

    Morgan para logar informações das rotas

--- config/custom-express.js
+++ config/custom-express.js
@@ -2,9 +2,19 @@ var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
 var validator = require('express-validator');
+var morgan = require('morgan');
+var logger = require('../services/logger.js');
 
 module.exports = function() {
   var app = express();
+
+  app.use(morgan('common', {
+    stream: {
+      write: function(msg) {
+        logger.info(msg);
+      }
+    }
+  }));
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
