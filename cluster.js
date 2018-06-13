ec24d8c728bea067ead23e3abbae62e012958526
Matheus Sartori <ms.sartori@outlook.com.br>
2018-06-12 23:59:50 -0300

    Cluster utilizado para subir app

--- cluster.js
+++ cluster.js
@@ -1,10 +1,18 @@
 var cluster = require('cluster');
+var os = require('os');
+
+var cpus = os.cpus();
 
 console.log('Running thread...');
 
 if(cluster.isMaster) {
   console.log('Running master thread...');
-  cluster.fork();
+
+  cpus.forEach(function() {
+    cluster.fork();
+  });
+
 } else {
   console.log('Running slave thread...');
+  require('./index.js');
 }
